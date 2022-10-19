9a1ef6b787137bc3c579ccf4cda1d70a0a7f91d3
mfavaret <mfavaret@my.centennialcollege.ca>
2022-10-19 12:19:11 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -5,5 +5,6 @@
 
  module.exports =
  {
-     "URI": "mongodb://127.0.0.1:27017/business_contacts"
+     //"URI": "mongodb://127.0.0.1:27017/business_contacts"
+     "URI": "mongodb+srv://marco:0LAAws89FQ65U4Tc@mongodbserver.xplevvh.mongodb.net/business_contacts?retryWrites=true&w=majority"
  }
